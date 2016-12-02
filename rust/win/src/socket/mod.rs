56f9a6ebd7cad4fde8c748ef6a868eb666c57f8b
Haiwei Li <haiweili@unity3d.com>
2016-12-02 18:12:34 +0800

    code

--- rust/win/src/socket/mod.rs
+++ rust/win/src/socket/mod.rs
@@ -1,4 +1,6 @@
 #![allow(dead_code)]
+use super::winapi;
+
 
 mod consts;
 mod ffi;
@@ -23,6 +25,19 @@ pub enum SockType {
     Rdm = consts::SOCK_RDM,
 }
 
+// use SetConsoleCtrlHandler
+pub unsafe extern "system" fn console_handler(ctrl_type: u32) -> i32 {
+    match ctrl_type {
+        winapi::wincon::CTRL_C_EVENT => println!("Ctrl-C pressed, exiting"),
+        winapi::wincon::CTRL_BREAK_EVENT => println!("Ctrl-Break pressed, exiting"),
+        winapi::wincon::CTRL_CLOSE_EVENT => println!("console closing, exiting"),
+        winapi::wincon::CTRL_LOGOFF_EVENT => println!("user logs off, exiting"),
+        winapi::wincon::CTRL_SHUTDOWN_EVENT => println!("Ctrl-shutdown pressed, exiting"),
+        _ => println!("Console Ctrl Handler: {}", ctrl_type),
+    }
+
+    return 1;
+}
 
 pub fn run() {
     process::Process::new();
--- rust/win/src/socket/process.rs
+++ rust/win/src/socket/process.rs
@@ -1,18 +1,13 @@
 use std;
-use std::{mem, ptr};
+use std::{mem, ptr, io};
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
 use winapi;
 use kernel32;
 use ws2_32;
-use super::{consts};
+use super::{console_handler};
 
-
-extern "system" {
-    fn console_handler(ctrlType: u32) -> i32 {
-        println!("Ctrl Type: {}", ctrlType);
-
-        return 1;
-    }
-}
 
 
 pub struct Process {}
@@ -26,6 +21,23 @@ impl Process {
         Process::info();
         Process::os_init();
         Process::set_console_handler();
+        Process::create_signal_events();
+        Process::create_process();
+
+        // loop input
+        /*
+        let mut input = String::new();
+        loop {
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+
+            match input.as_str() {
+                "q" => break,
+                _ => println!("You input: {}", input),
+            }
+        }
+        */
     }
 
     pub fn info() {
@@ -79,9 +91,103 @@ impl Process {
     }
 
     pub fn set_console_handler() {
-        match unsafe { kernel32::SetConsoleCtrlHandler(Some(Process::console_handler), 1) } {
+        match unsafe { kernel32::SetConsoleCtrlHandler(Some(console_handler), 1) } {
             0 => println!("Set Console Ctrl Handler failed!"),
             _ => println!("Set Console Ctrl Handler done"),
+        }
+    }
+
+    pub fn to_wchar(str : &str) -> *const u16 {
+        let v : Vec<u16> = OsStr::new(str).encode_wide().chain(Some(0).into_iter()).collect();
+
+        v.as_ptr()
+    }
+
+    pub fn create_signal_events() {
+
+        println!("Create events ---------------------------------");
+
+        let stop_event_name = Process::to_wchar("stop_event");
+        let stop_event = unsafe { kernel32::CreateEventW(ptr::null_mut(), 1, 0, stop_event_name) };
+        if stop_event.is_null() {
+            println!("Create stop_event failed!");
+        } else {
+            println!("Create stop_event");
+        }
+
+        let quit_event_name = Process::to_wchar("quit_event");
+        let quit_event = unsafe { kernel32::CreateEventW(ptr::null_mut(), 1, 0, quit_event_name) };
+        if quit_event.is_null() {
+            println!("Create quit_event failed!");
+        } else {
+            println!("Create quit_event");
+        }
+
+        let reopen_event_name = Process::to_wchar("quit_event");
+        let reopen_event = unsafe { kernel32::CreateEventW(ptr::null_mut(), 1, 0, reopen_event_name) };
+        if reopen_event.is_null() {
+            println!("Create reopen_event failed!");
+        } else {
+            println!("Create reopen_event");
+        }
+
+        let reload_event_name = Process::to_wchar("quit_event");
+        let reload_event = unsafe { kernel32::CreateEventW(ptr::null_mut(), 1, 0, reload_event_name) };
+        if reload_event.is_null() {
+            println!("Create reload_event failed!");
+        } else {
+            println!("Create reload_event");
+        }
+
+        unsafe {
+            kernel32::CloseHandle(stop_event);
+            kernel32::CloseHandle(quit_event);
+            kernel32::CloseHandle(reopen_event);
+            kernel32::CloseHandle(reload_event);
+        }
+    }
+
+    pub fn create_process() {
+        println!("Create process ---------------------------------");
+
+        for x in 0..2 {
+            println!("Process: {:?}", x);
+
+
+            let mut program = std::env::current_exe().unwrap();
+            println!("Program path: {:?}", program);
+
+            let mut statu_info: winapi::processthreadsapi::STARTUPINFOW;
+            let mut process_info: winapi::processthreadsapi::PROCESS_INFORMATION;
+
+            unsafe {
+                statu_info = mem::zeroed::<winapi::processthreadsapi::STARTUPINFOW>();
+                process_info = mem::zeroed::<winapi::processthreadsapi::PROCESS_INFORMATION>();
+
+                let mut path = Process::to_wchar(program.to_str().unwrap()) as *mut u16;
+
+                match kernel32::CreateProcessW(
+                    ptr::null_mut(),
+                    path,
+                    ptr::null_mut(),
+                    ptr::null_mut(),
+                    0,
+                    winapi::winbase::CREATE_NO_WINDOW,
+                    ptr::null_mut(),
+                    ptr::null_mut(),
+                    &mut statu_info,
+                    &mut process_info) {
+                        0 => {
+                            println!("Create process failed!");
+                            println!("Status info: {:?}", &statu_info);
+                            println!("Process info: {:?}", &process_info);
+                        },
+                        _ => {
+                            kernel32::CloseHandle(process_info.hProcess);
+                            kernel32::CloseHandle(process_info.hThread);
+                        },
+                }
+            }
         }
     }
 
